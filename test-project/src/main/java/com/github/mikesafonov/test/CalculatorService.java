afcdb751db87a524264b43e59a62593661f67836
msafonov <msafonovmail@gmail.com>
2023-02-20 20:59:32 +0300

    test

--- test-project/src/main/java/com/github/mikesafonov/test/CalculatorService.java
+++ test-project/src/main/java/com/github/mikesafonov/test/CalculatorService.java
@@ -5,6 +5,7 @@ public class CalculatorService {
     public int calc(int a, int b) {
         int sum = a + b;
         if (a > b) {
+            a++;
             return sum * a;
         }
         return sum * b;
