9eac47b081e979f9feb8e7267ee748677121c18e
msafonov <msafonovmail@gmail.com>
2023-03-26 13:09:07 +0300

    fix pitest-git and pitest-git-changes-report-plugin publications

--- pitest-git-changes-report-plugin/build.gradle.kts
+++ pitest-git-changes-report-plugin/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java-library")
+    id("maven-publish")
+    id("signing")
 }
 
 group = "com.github.mikesafonov"
@@ -43,4 +45,48 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            pom {
+                name.set("pitest-git-changes-report-plugin")
+                url.set("https://github.com/MikeSafonov/pitest-git-changes-plugin")
+                description.set("Pitest plugin for reporting mutating git changes")
+                organization {
+                    name.set("com.github.mikesafonov")
+                    url.set("https://github.com/MikeSafonov")
+                }
+                issueManagement {
+                    system.set("GitHub")
+                    url.set("https://github.com/MikeSafonov/pitest-git-changes-plugin/issues")
+                }
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://github.com/MikeSafonov/pitest-git-changes-plugin/blob/master/LICENSE")
+                        distribution.set("repo")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/MikeSafonov/pitest-git-changes-plugin")
+                    connection.set("scm:git:git://github.com/MikeSafonov/pitest-git-changes-plugin.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:MikeSafonov/pitest-git-changes-plugin.git")
+                }
+                developers {
+                    developer {
+                        name.set("Mike Safonov")
+                        organization.set("com.github.mikesafonov")
+                        organizationUrl.set("https://github.com/MikeSafonov")
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
--- pitest-git/build.gradle.kts
+++ pitest-git/build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     id("java-library")
+    id("maven-publish")
+    id("signing")
 }
+
+group = "com.github.mikesafonov"
+version = project.properties["pluginVersion"]!!
 
 repositories {
     mavenCentral()
@@ -11,6 +16,8 @@ tasks.jar {
 }
 
 java {
+    withJavadocJar()
+    withSourcesJar()
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(8))
     }
@@ -27,4 +34,48 @@ dependencies {
     val lombokVersion = "1.18.26"
     compileOnly("org.projectlombok:lombok:$lombokVersion")
     annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            pom {
+                name.set("pitest-git")
+                url.set("https://github.com/MikeSafonov/pitest-git-changes-plugin")
+                description.set("Pitest git integration")
+                organization {
+                    name.set("com.github.mikesafonov")
+                    url.set("https://github.com/MikeSafonov")
+                }
+                issueManagement {
+                    system.set("GitHub")
+                    url.set("https://github.com/MikeSafonov/pitest-git-changes-plugin/issues")
+                }
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://github.com/MikeSafonov/pitest-git-changes-plugin/blob/master/LICENSE")
+                        distribution.set("repo")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/MikeSafonov/pitest-git-changes-plugin")
+                    connection.set("scm:git:git://github.com/MikeSafonov/pitest-git-changes-plugin.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:MikeSafonov/pitest-git-changes-plugin.git")
+                }
+                developers {
+                    developer {
+                        name.set("Mike Safonov")
+                        organization.set("com.github.mikesafonov")
+                        organizationUrl.set("https://github.com/MikeSafonov")
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
